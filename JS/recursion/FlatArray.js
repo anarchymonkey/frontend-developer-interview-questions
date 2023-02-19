5e84dcce7e0a79a2e81cacfb86e7ff52f1241708
Priyang <patelpriyang95@gmail.com>
2023-02-19 10:03:01 +0000

    fixed Depth

--- JS/recursion/FlatArray.js
+++ JS/recursion/FlatArray.js
@@ -7,21 +7,17 @@
 
 */
 
-export const FlatArray = (InputArr, Depth = Infinity) => {
+export const FlatArray = (InputArr, Depth = Infinity, CurrentDepth = 0) => {
   let flattened = [];
-
+  const GlobalDepth = Depth;
   for (let i = 0; i < InputArr.length; i++) {
     const element = InputArr[i];
     if (Array.isArray(element) && element.length > 0) {
-      let CurrentDepth = 0;
-      if (Depth <= CurrentDepth) {
-        console.log(element);
+      if (GlobalDepth === CurrentDepth) {
         flattened.push(element);
       } else {
-        const Flatten = FlatArray(element);
-        CurrentDepth += 1;
+        const Flatten = FlatArray(element, GlobalDepth, CurrentDepth + 1);
         flattened = flattened.concat(Flatten);
-        //   flattened.push(...Flatten); we can use spread operator as well.
       }
     } else {
       flattened.push(element);
